044e8b5e2c78f3e619a69f13ece750245e80e4c4
joarley Santos <joarley.santos@vidisoftware.com>
2013-08-27 19:32:54 -0300

    .

--- tools/packet analyzer/RylPacketAnalyzerV2/Model/NetworkComunication/SourceAddress.cs
+++ tools/packet analyzer/RylPacketAnalyzerV2/Model/NetworkComunication/SourceAddress.cs
@@ -1,44 +1,32 @@
 ﻿namespace RylPacketAnalyzerV2.Model.NetworkComunication
 {
-    using System;
     using System.ComponentModel;
 
-    public class SourceAddress : INotifyPropertyChanged
+    public struct SourceAddress : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        Guid id;
         int port;
         SourceAddressType type;
 
-        public Guid Id
-        {
-            get { return id; }
-            set { id = value; OnPropertyChanged("Id"); }
-        }
         public int Port
         {
             get { return port; }
             set { port = value; OnPropertyChanged("Port"); }
         }
+
         public SourceAddressType Type
         {
             get { return type; }
             set { type = value; OnPropertyChanged("Type"); }
         }
 
-        public SourceAddress() : this(new Guid()) { }
-
-        public SourceAddress(Guid id)
-        {
-            this.Id = id;
-        }
-
         public static bool operator ==(SourceAddress a, SourceAddress b)
         {
             if (a == null && b == null) return true;
             if (a == null || b == null) return false;
-            return a.Id == b.Id;
+            return a.Port == b.Port &&
+                a.type == b.type;
         }
 
 
